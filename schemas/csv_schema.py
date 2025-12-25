a3f78e5d2cb9c14cd0aa68b9884f875a4c30200f
johninglis775-stack <johninglis775@gmail.com>
2025-12-25 01:11:29 +0000

    Create csv_schema.py

---
+++ schemas/csv_schema.py
@@ -0,0 +1,15 @@
+CSV_HEADERS = [
+    "Season",
+    "Competition",
+    "GameWeek",
+    "FixtureId",
+    "OddsFetchTimestampUTC",
+    "MatchDate",
+    "KickoffTimeUTC",
+    "KickoffDateTimeUTC",
+    "KickoffTimeLocal",
+    "HomeTeam",
+    "AwayTeam",
+    "HomeTeamId",
+    "AwayTeamId"
+]
