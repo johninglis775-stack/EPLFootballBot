fd480949e11f2b7c63b82136a97203698768bb3a
johninglis775-stack <johninglis775@gmail.com>
2025-12-25 01:12:28 +0000

    Create coverage_schema.py

---
+++ schemas/coverage_schema.py
@@ -0,0 +1,10 @@
+def empty_coverage():
+    return {
+        "run_date_utc": None,
+        "season": None,
+        "gameweek": None,
+        "expected_fixtures": 0,
+        "fetched_fixtures": 0,
+        "status": None,
+        "status_reason": None
+    }
